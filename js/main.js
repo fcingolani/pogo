185b532bfd724fd2da24aa94844a73a3ef1f87ce
Federico Cingolani <mail@fcingolani.com.ar>
2019-07-28 00:17:32 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const whiteCircledNumbers = ['⓪','①','②','③','④','⑤','⑥','⑦','⑧','⑨','⑩','⑪','⑫','⑬','⑭','⑮'];
+
+Vue.component('iv-picker', {
+  props: ['value'],
+  template: '#iv-picker'
+})
+
+let app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!',
+    attack: 5,
+    defense: 5,
+    health: 5,
+  },
+
+  mounted() {
+    new ClipboardJS(this.$refs.copyButton, {
+      text: () => this.name
+    });
+  },
+
+  computed: {
+    ivPercentage() {
+      let pct = (this.attack+this.defense+this.health) / 45;
+      return Math.round(pct * 100);
+    },
+    name() {
+      return [
+        this.ivPercentage < 100 ? this.ivPercentage : '!!',
+        whiteCircledNumbers[this.attack],
+        whiteCircledNumbers[this.defense],
+        whiteCircledNumbers[this.health]
+      ].join('');
+    }
+  }
+})
